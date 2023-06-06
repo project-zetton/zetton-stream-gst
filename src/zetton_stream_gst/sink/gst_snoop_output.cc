a3cf3ab4e8d853ae593743ff86cd8a0e7a8afb96
corenel <xxdsox@gmail.com>
2023-06-06 09:23:30 +0800

    refactor(sink): replace NULL with nullptr

--- src/zetton_stream_gst/sink/gst_snoop_output.cc
+++ src/zetton_stream_gst/sink/gst_snoop_output.cc
@@ -1,6 +1,7 @@
 #include "zetton_stream_gst/sink/gst_snoop_output.h"
 
 #include <functional>
+#include <thread>
 #include <utility>
 
 GstSnoopOutput::GstSnoopOutput(std::string pipeline_src,
@@ -15,16 +16,16 @@ GstSnoopOutput::~GstSnoopOutput() { Stop(); }
 
 bool GstSnoopOutput::Init() {
   // Initializes the GMainLoop
-  loop_ = g_main_loop_new(NULL, FALSE);
+  loop_ = g_main_loop_new(nullptr, FALSE);
 
   // Setting up source pipeline, we read from a file and convert to our desired
-  gchar *string = NULL;
+  gchar *string = nullptr;
   string = g_strdup_printf("%s", pipeline_src_.c_str());
-  src_ = gst_parse_launch(string, NULL);
+  src_ = gst_parse_launch(string, nullptr);
   g_free(string);
 
   // If source pipeline is faulty, unref loop and return false
-  if (src_ == NULL) {
+  if (src_ == nullptr) {
     g_print("Bad source\n");
     g_main_loop_unref(loop_);
     return false;
@@ -33,16 +34,17 @@ bool GstSnoopOutput::Init() {
   // g_print("Capture bin launched\n");
 
   // to be notified of messages from this pipeline, mostly EOS
-  GstBus *bus = NULL;
+  GstBus *bus = nullptr;
   bus = gst_element_get_bus(src_);
   gst_bus_add_watch(bus, (GstBusFunc)OnSourceMessage, this);
   gst_object_unref(bus);
 
   // we use appsink in push mode, it sends us a signal when data is available
   // and we pull out the data in the signal callback.
-  GstElement *testsink = NULL;
+  GstElement *testsink = nullptr;
   testsink = gst_bin_get_by_name(GST_BIN(src_), "testsink");
-  g_object_set(G_OBJECT(testsink), "emit-signals", TRUE, "sync", FALSE, NULL);
+  g_object_set(G_OBJECT(testsink), "emit-signals", TRUE, "sync", FALSE,
+               nullptr);
   g_signal_connect(testsink, "new-sample", G_CALLBACK(OnNewSampleFromSink),
                    this);
   gst_object_unref(testsink);
@@ -52,11 +54,11 @@ bool GstSnoopOutput::Init() {
   // behaviour on the src which means that we will push the entire file into
   // memory.
   string = g_strdup_printf("%s", pipeline_sink_.c_str());
-  sink_ = gst_parse_launch(string, NULL);
+  sink_ = gst_parse_launch(string, nullptr);
   g_free(string);
 
   // If sink pipeline is faulty, unref source pipeline and loop and return false
-  if (sink_ == NULL) {
+  if (sink_ == nullptr) {
     g_print("Bad sink\n");
     gst_object_unref(src_);
     g_main_loop_unref(loop_);
@@ -65,12 +67,12 @@ bool GstSnoopOutput::Init() {
   // g_print("Play bin launched\n");
 
   // Obtains the testsource element by name from the bin
-  GstElement *testsource = NULL;
+  GstElement *testsource = nullptr;
   testsource = gst_bin_get_by_name(GST_BIN(sink_), "testsource");
   // configures the source to push data in time-based format
-  g_object_set(testsource, "format", GST_FORMAT_TIME, NULL);
+  g_object_set(testsource, "format", GST_FORMAT_TIME, nullptr);
   // uncomment this line to block when appsrc has buffered enough
-  g_object_set(testsource, "block", TRUE, NULL);
+  g_object_set(testsource, "block", TRUE, nullptr);
   gst_object_unref(testsource);
 
   // Adds a watch on the bus to receive messages from the sink pipeline, mostly
@@ -99,7 +101,7 @@ void GstSnoopOutput::Start() {
 }
 
 void GstSnoopOutput::Stop() {
-  // set the source and sink pipelines to NULL
+  // set the source and sink pipelines to nullptr
   gst_element_set_state(src_, GST_STATE_NULL);
   gst_element_set_state(sink_, GST_STATE_NULL);
 
